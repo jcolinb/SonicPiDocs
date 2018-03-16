c6f4f88e9d6ff66d724ab2e58a99dd782eb3f020
c mulligan <gateless@protonmail.com>
2018-03-16 11:31:24 -0400

    markdown load test

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$('.container').load("entries.html .entries");
+$('.container').load("https://raw.githubusercontent.com/samaaron/sonic-pi/master/etc/doc/tutorial/01-Welcome-to-Sonic-Pi.md");
 
 $('.container').click('.entries',function (event) {
   $(event.target).find('.content').toggleClass('hidden');
